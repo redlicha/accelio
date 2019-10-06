d68327cc0339158aa868e6f75e6cdc6290bacf44
Eyal Salomon <eyals@iguazio.com>
2019-10-06 09:18:48 +0300

    accelio: change XIO_E_INSUFFICIENT_RESOURCES error text to "Application resources are temporarily unavailable"
    Signed-off-by: Eyal Salomon <eyals@iguazio.com>

--- src/common/xio_error.c
+++ src/common/xio_error.c
@@ -126,7 +126,7 @@ static const char *xio_gen_status_str(enum xio_status ev)
 	case XIO_E_RSP_BUF_SIZE_MISMATCH:
 		return "Response buffer is smaller then actual response";
 	case XIO_E_INSUFFICIENT_RESOURCES:
-		return "Insufficient resources to reply";
+		return "Application resources are temporary unavailable";
 	default:
 		return "Unknown error";
 	};
