9632499b1b5f9e8804b7082a1906f0037a6cc666
Yan Burman <yanburman@users.noreply.github.com>
2022-04-20 08:47:23 +0300

    tcp: Add error print in case of recvmsg error
    Signed-off-by: Yan Burman <yanburman@users.noreply.github.com>

--- src/usr/transport/tcp/xio_tcp_datapath.c
+++ src/usr/transport/tcp/xio_tcp_datapath.c
@@ -3190,6 +3190,7 @@ int xio_tcp_rx_data_handler(struct xio_tcp_transport *tcp_hndl, int batch_nr)
 			return -1;
 		} else if (recvmsg_retval < 0) {
 			if (xio_errno() != XIO_EAGAIN) {
+				ERROR_LOG("recvmsg failed:%d. Disconnecting\n", xio_errno());
 				xio_tcp_disconnect_helper(tcp_hndl);
 			}
 			break;
